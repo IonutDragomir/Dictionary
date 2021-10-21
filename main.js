40bf4061d1d923ad0de45d72daca041347a21e77
IonutDragomir <72976711+IonutDragomir@users.noreply.github.com>
2021-10-21 13:32:29 +0300

    Added search functionality

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
+let book = [];
 
-function winner(number) {
-  return winningButton = Math.floor(Math.random() * number);
+
+function collect(id) {
+    return(document.getElementById(id).value);
 }
 
-function playerChoice(buttonId, winingButton) {
-  if(buttonId == winingButton) {
-    document.getElementById("WINNER/LOSER").innerHTML = "<div class='won'><p>You won!!</p></div>";
-    // return alert('You won!');
-  } else {
-    document.getElementById("WINNER/LOSER").innerHTML = "<div class='lose'><p>You lose!!</p></div>";
-    // return alert('You lose');
-  }
-}
+function dictionary() {
+    let isWord = collect("field");
+    let contor = 0;
+    for( let index = 0; index < book.length; ++index) {
+        if(isWord === book[index]) {
+            contor = 1;
+        }
 
-function numberOfButtons() {
-    const boxValue = document.getElementById("NumberBox").value;
-    let sameValue = boxValue;
-    let index = 0;
-    let winingButton = winner(boxValue);
-    while (sameValue > 0) {
-        document.getElementById("WINNER/LOSER").innerHTML +=
-        `<button type="button" onclick="return playerChoice(${index}, ${winingButton});">Press me</button>`
-        ++index;
-        --sameValue;
     }
+    if (contor === 0) {
+        book.push(isWord);
+    } else {
+        document.getElementById("response").innerHTML += `<p>The word you are trying to add is already in dictionary</p>`
+    }
+}
+
+function searchWord() {
+    let isWord = collect("searchPlace");
+    let contor = 0;
+    for( let index = 0; index < book.length; ++index) {
+        if(isWord === book[index]) {
+            contor = 1;
+        }
+    }
+    if (contor === 0) {
+        document.getElementById("response").innerHTML += `<p>The word you are searching is not in dictionary</p>`
+    } else {
+        document.getElementById("response").innerHTML += `<p>The word you are searching is in dictionary</p>`
+    }
+}
+
+
+function moreWords() {
+    document.getElementById("response").innerHTML += `<p>${book}</p>`
 }
